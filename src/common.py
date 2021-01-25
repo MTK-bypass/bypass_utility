faf4b33292c2fede39925fe32cb592797618cfc1
Chaosmaster <chaosmaster@chaosv.de>
2021-01-25 16:18:27 +0100

    endin => endian

--- src/common.py
+++ src/common.py
@@ -5,17 +5,17 @@ def raise_(ex):
     raise ex
 
 
-def to_bytes(value, size=1, endin='>'):
+def to_bytes(value, size=1, endian='>'):
     return {
-        1: lambda: struct.pack(endin + 'B', value),
-        2: lambda: struct.pack(endin + 'H', value),
-        4: lambda: struct.pack(endin + 'I', value)
+        1: lambda: struct.pack(endian + 'B', value),
+        2: lambda: struct.pack(endian + 'H', value),
+        4: lambda: struct.pack(endian + 'I', value)
     }.get(size, lambda: raise_(RuntimeError("invalid size")))()
 
 
-def from_bytes(value, size=1, endin='>'):
+def from_bytes(value, size=1, endian='>'):
     return {
-        1: lambda: struct.unpack(endin + 'B', value)[0],
-        2: lambda: struct.unpack(endin + 'H', value)[0],
-        4: lambda: struct.unpack(endin + 'I', value)[0]
+        1: lambda: struct.unpack(endian + 'B', value)[0],
+        2: lambda: struct.unpack(endian + 'H', value)[0],
+        4: lambda: struct.unpack(endian + 'I', value)[0]
     }.get(size, lambda: raise_(RuntimeError("invalid size")))()
